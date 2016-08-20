f594e0daf2ea6628cc4e3093bc55b7219374173d
MRVcoin <MRVcoin@hotmail.com>
2016-08-20 11:18:47 +0800

    Add files via upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Marvelous-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "MarvelousURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
